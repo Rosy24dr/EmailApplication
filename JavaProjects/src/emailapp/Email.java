4878cec42eef3183b36b028e63d30a18a73f30b7
Rosy24dr <Rosy24dr@outlook.com>
2022-06-28 23:28:55 -0400

    created the constructor in the Email Class file, created the object in the EmailApp Class file

--- JavaProjects/src/emailapp/Email.java
+++ JavaProjects/src/emailapp/Email.java
@@ -1,5 +1,19 @@
 package emailapp;
 
 public class Email {
+	 String firstName;
+	 String lastName;
+	 String passWord;
+	 String department; 
+	 int mailboxCapacity;
+	 String alternateEmail;
+	 
+	 public Email(String firstName, String lastName) {
+		 this.firstName = firstName;
+		 this.lastName = lastName;
+		 System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+		 
+	 }
+	
 
 }
